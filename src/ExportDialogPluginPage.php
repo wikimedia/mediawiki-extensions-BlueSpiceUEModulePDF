b08ea046cfcd0c3d1ed9e44b04e68e3dcad04f66
dvogel <daniel.vogel@hallowelt.com>
2022-05-20 23:38:09 +0200

    Universal export dialog plugin
    [4.2]
    
    ERM19212
    
    Depends-On: Ic450dccde577d76f8e8eef20f9dae66e39a4f4cc
    
    Change-Id: I675aabdfe7c1a24dcbbbf5c11742dbc3c1b52c43

---
+++ src/ExportDialogPluginPage.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace BlueSpice\UEModulePDF;
+
+use BlueSpice\UniversalExport\IExportDialogPlugin;
+use IContextSource;
+use MediaWiki\MediaWikiServices;
+
+class ExportDialogPluginPage implements IExportDialogPlugin {
+
+	/**
+	 * @return void
+	 */
+	public static function factory() {
+		return new static();
+	}
+
+	/**
+	 *
+	 * @return array
+	 */
+	public function getRLModules(): array {
+		return [ "ext.bluespice.ueModulePDF.ue-export-dialog-plugin.pdf" ];
+	}
+
+	/**
+	 *
+	 * @return array
+	 */
+	public function getJsConfigVars(): array {
+		$services = MediaWikiServices::getInstance();
+		$config = $services->getConfigFactory()->makeConfig( 'bsg' );
+
+		$defaultTemplate = $config->get( 'UEModulePDFDefaultTemplate' );
+		$defaultTemplatePath = $config->get( 'UEModulePDFTemplatePath' );
+
+		$availableTemplates = [];
+		$dir = opendir( $defaultTemplatePath );
+		$subDir = readdir( $dir );
+		while ( $subDir !== false ) {
+			if ( in_array( $subDir, [ '.', '..', 'common' ] ) ) {
+				$subDir = readdir( $dir );
+				continue;
+			}
+
+			if ( !is_dir( "{$defaultTemplatePath}/{$subDir}" ) ) {
+				$subDir = readdir( $dir );
+				continue;
+			}
+
+			if ( file_exists( "{$defaultTemplatePath}/{$subDir}/template.php" ) ) {
+				$availableTemplates[] = $subDir;
+			}
+
+			$subDir = readdir( $dir );
+		}
+
+		$jsConfigVars = [];
+		if ( empty( $availableTemplates ) ) {
+			$defaultTemplate = '';
+		} else {
+			if ( !in_array( $defaultTemplate, $availableTemplates ) ) {
+				$defaultTemplate = $availableTemplates[0];
+			}
+		}
+
+		$jsConfigVars = [
+			'bsUEModulePDFDefaultTemplate' => $defaultTemplate,
+			'bsUEModulePDFAvailableTemplates' => $availableTemplates
+		];
+
+		return $jsConfigVars;
+	}
+
+	/**
+	 *
+	 * @param IContextSource $context
+	 * @return bool
+	 */
+	public function skip( IContextSource $context ): bool {
+		return false;
+	}
+}
