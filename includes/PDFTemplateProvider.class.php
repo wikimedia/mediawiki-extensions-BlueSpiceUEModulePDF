1bb43aa7f65a4b48aaf3df4a4e46010207006c09
libraryupgrader <tools.libraryupgrader@tools.wmflabs.org>
2022-11-16 01:07:42 +0000

    build: Updating mediawiki/mediawiki-codesniffer to 40.0.1
    Change-Id: Ie8fefb8c79d90d3f113b3e068538d68c9cdbe763

--- includes/PDFTemplateProvider.class.php
+++ includes/PDFTemplateProvider.class.php
@@ -45,8 +45,7 @@ class BsPDFTemplateProvider {
 				$sDirName = $oFileInfo->getBasename();
 				$aOptions[$aTemplate['info']['name']] = $sDirName;
 			}
-		}
-		catch ( Exception $e ) {
+		} catch ( Exception $e ) {
 			wfDebugLog( 'BS::UEModulePDF', 'BsPDFTemplateProvider::getTemplatesForSelectOptions: Error: ' . $e->getMessage() );
 			return [ '-' => '-' ];
 		}
